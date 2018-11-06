9d31a991c25b62b86f9888bbb5871880bd143e19
James Henshaw <james.henshaw@trint.com>
2018-11-06 12:12:38 +0000

    Implement basic stratgies using request auth header

--- strategies/admin.js
+++ strategies/admin.js
@@ -1,3 +1,14 @@
-const adminStrategy = () => true;
+const ADMIN_SECRET = 'JsRoundaboutAdmin';
+
+const adminStrategy = (requestData = {}) => {
+  const { headers } = requestData;
+
+  if (!headers || !headers.authorization) {
+    return false;
+  }
+
+  const authHeader = headers.authorization;
+  return authHeader === ADMIN_SECRET;
+};
 
 module.exports = adminStrategy;
--- strategies/user.js
+++ strategies/user.js
@@ -1,3 +1,14 @@
-const userStrategy = () => true;
+const USER_SECRET = 'JsRoundabout';
+
+const userStrategy = (requestData = {}) => {
+  const { headers } = requestData;
+
+  if (!headers || !headers.authorization) {
+    return false;
+  }
+
+  const authHeader = headers.authorization;
+  return authHeader === USER_SECRET;
+};
 
 module.exports = userStrategy;
